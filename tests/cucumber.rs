d5ea6146575e7fb5fe044f7571fb5c07b3f16961
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2020-04-27 13:08:12 +0200

    test: dataset creation

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -2,36 +2,22 @@ mod knowledge_steps;
 
 use cucumber::cucumber;
 use tooling::knowledge::Dataset;
-use tooling::knowledge::FusekiKnowledgeService;
-use tooling::knowledge::KnowledgeService;
 
 pub struct MyWorld {
-    foo: String,
     dataset: Option<Dataset>,
 }
 
 impl cucumber::World for MyWorld {}
 impl std::default::Default for MyWorld {
     fn default() -> MyWorld {
-        // let client = reqwest::Client::new();
-        // let local = url::Url::parse("http://localhost:3030/").unwrap();
-        // let knowledge = FusekiKnowledgeService {
-        //     client: &client,
-        //     base: local,
-        // };
-
         MyWorld {
-            foo: "a default string".to_string(),
             dataset: None::<Dataset>,
-            // knowledge,
         }
     }
 }
 
 cucumber! {
-    features: "./features", // Path to our feature files
-    world: crate::MyWorld, // The world needs to be the same for knowledge_steps and the main cucumber call
-    steps: &[
-        knowledge_steps::steps // the `knowledge_steps!` macro creates a `knowledge_steps` function in a module
-    ]
+    features: "./features",
+    world: crate::MyWorld,
+    steps: &[knowledge_steps::steps]
 }
--- tests/knowledge_steps/mod.rs
+++ tests/knowledge_steps/mod.rs
@@ -1,22 +1,28 @@
-use cucumber::steps;
+use cucumber::{Steps, StepsBuilder};
+use tokio_test::block_on;
 use tooling::knowledge::FusekiKnowledgeService;
 use tooling::knowledge::KnowledgeService;
 
-// Any type that implements cucumber::World + Default can be the world
-steps!(crate::MyWorld => {
+pub fn steps() -> Steps<crate::MyWorld> {
+    let mut builder: StepsBuilder<crate::MyWorld> = StepsBuilder::new();
 
-    given "I have no dataset" |world, step| {
-        world.dataset = None;
-    };
+    builder
+        .given("I have no dataset", |world, _step| {
+            world.dataset = None;
+        })
+        .when("I create a temporary dataset", |world, _step| {
+            let client = reqwest::Client::new();
+            let local = url::Url::parse("http://localhost:3030/").unwrap();
+            let knowledge = FusekiKnowledgeService {
+                client: &client,
+                base: local,
+            };
+            let dataset = block_on(knowledge.create_temporary_dataset());
+            world.dataset = Some(dataset);
+        })
+        .then("I have a dataset", |world, _step| {
+            assert!(world.dataset.is_some())
+        });
 
-    when "I create a temporary dataset" |world, step| {
-        let client = reqwest::Client::new();
-        let local = url::Url::parse("http://localhost:3030/").unwrap();
-        let knowledge = FusekiKnowledgeService {
-            client: &client,
-            base: local,
-        };
-        let dataset = knowledge.create_temporary_dataset();
-        // world.dataset = Some(dataset);
-    };
-});
+    builder.build()
+}
